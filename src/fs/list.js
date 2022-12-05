0a8d38734a224399321c7bae03adbf2a49c91fb5
e.malafeev <e.malafeev@vk.team>
2022-12-05 20:53:36 +0600

    add solution for list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir, access } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const path = 'src/fs/files';
+    try {
+        await access(path);
+        const files = await readdir(path);
+        console.log(files)
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await list();
