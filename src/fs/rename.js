5fb01d2082eda1e322c6a4f2b5d107ec6f652057
e.malafeev <e.malafeev@vk.team>
2022-12-05 21:23:41 +0600

    add solution for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import { rename as fsRename, access } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = 'src/fs/files/wrongFilename.txt';
+    const newPath = 'src/fs/files/properFilename.md';
+    try {
+        await Promise.allSettled([access(oldPath), access(newPath)]);
+        await fsRename(oldPath, newPath);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await rename();
